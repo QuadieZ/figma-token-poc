06cefb4d23bbb202644cc89b182dd41efcec26bf
quadiez <dduidokdoi@gmail.com>
2022-11-02 23:01:01 -0400

    feat: update token

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mui/material';
+import { Box, Button, styled, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+
+const BannerTitle = styled(Typography)(({ theme }) => ({
+  color: 'var(--content-primary)',
+  fontSize: 'var(--fontsize-1)',
+  fontWeight: 'var(--fontweight-1)',
+  fontFamily: 'Poppins'
+}))
 
 function App() {
   return (
-    <Button sx={{
-      color: 'var(--global-brks-core-color-grey-50)',
-      backgroundColor: 'var(--global-brks-core-color-grey-900)'
-    }}>Text</Button>
+    <Stack p={10} spacing={4}>
+      <BannerTitle>Hello, I am Primary Title!</BannerTitle>
+      <Button sx={{
+        color: 'var(--brks-core-color-grey-50)',
+        backgroundColor: 'var(--brks-core-color-grey-900)',
+        borderRadius: 'var(--border-md)',
+        width: 'fit-content',
+      }}>Button</Button>
+    </Stack>
   );
 }
 
